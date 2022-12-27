955cab54bdc6d8eced8b6c3f5eb88fd0684191f2
Gabriel dos Santos Oliveira <gabrielsantosoliveira951@gmail.com>
2022-12-26 22:09:39 -0400

    feat: Add server

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,12 @@
 import express from 'express';
 
+import 'dotenv/config';
 import { routes } from './routes';
-
+import './database';
 const app = express();
 app.use(express.json());
 app.use(routes);
-const PORT = 3333;
+const PORT = process.env.PORT ?? 3333;
 app.listen(PORT, () => {
   console.log(`http://localhost:${PORT}`);
 });
