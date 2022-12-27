2c9d744dcf1412fdef9721fbff71da51c5ba5a1a
Gabriel dos Santos Oliveira <gabrielsantosoliveira951@gmail.com>
2022-12-26 22:12:58 -0400

    feat: Add file index with connection for database

--- src/database/index.ts
+++ src/database/index.ts
@@ -1,5 +1,14 @@
 import Sequelize from 'sequelize';
 
+import { Address } from '../models/Address';
+import { Tech } from '../models/Tech';
+import { User } from '../models/User';
 import * as dbConfig from './../config/database';
 const connection = new Sequelize.Sequelize(dbConfig as any);
+Tech.initModel(connection);
+User.initModel(connection);
+Address.initModel(connection);
+Tech.associate({ models: connection.models });
+Address.associate({ models: connection.models });
+User.associate({ models: connection.models });
 export { connection };
