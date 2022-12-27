1fd076341bebfe324b2ea64c55c8ef4986b7d72d
Gabriel dos Santos Oliveira <gabrielsantosoliveira951@gmail.com>
2022-12-26 22:09:22 -0400

    feat: Add routes

--- src/routes.ts
+++ src/routes.ts
@@ -1,8 +1,28 @@
 import { Router } from 'express';
 
+import { AddressController } from './controllers/AddressController';
+import { ReportController } from './controllers/ReportController';
+import { TechController } from './controllers/TechController';
+import { UserController } from './controllers/UserController';
 const routes = Router();
+routes.get('/users', UserController.findAll);
+routes.post('/users', UserController.store);
 
-routes.get('/', (req, res) => {
-  return res.json({ hello: 'Hello World' });
-});
+routes.get(
+  '/users/:user_id/addresses',
+  AddressController.index
+);
+routes.post(
+  '/users/:user_id/addresses',
+  AddressController.store
+);
+
+routes.get('/users/:user_id/techs', TechController.index);
+routes.post('/users/:user_id/techs', TechController.store);
+routes.delete(
+  '/users/:user_id/techs',
+  TechController.delete
+);
+
+routes.get('/report', ReportController.show);
 export { routes };
