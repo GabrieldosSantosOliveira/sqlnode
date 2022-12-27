fdfd51162e2f30bb5b4d290ff28472e6adb26ff5
Gabriel dos Santos Oliveira <gabrielsantosoliveira951@gmail.com>
2022-12-26 22:14:02 -0400

    feat: Add migration for create table techs

---
+++ src/database/migrations/20221217175624-create-techs.js
@@ -0,0 +1,33 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('techs', {
+      id: {
+        type: Sequelize.UUID,
+        primaryKey: true,
+        allowNull: false,
+        defaultValue: Sequelize.UUIDV4
+      },
+      name: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      created_at: {
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.fn('NOW'),
+        allowNull: false
+      },
+      updated_at: {
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.fn('NOW'),
+        allowNull: false
+      }
+    });
+  },
+
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('techs');
+  }
+};
