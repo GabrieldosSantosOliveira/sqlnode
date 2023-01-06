5faa73bafb2ae39772b8edebab8720da0484127b
Gabriel dos Santos Oliveira <gabrielsantosoliveira951@gmail.com>
2023-01-06 17:05:20 -0400

    feat: Add try catch

--- src/controllers/ReportController.ts
+++ src/controllers/ReportController.ts
@@ -1,34 +1,36 @@
-import {
-  Request,
-  Response
-} from 'express-serve-static-core';
+import { Request, Response } from 'express';
 import { Op } from 'sequelize';
 
 import { User } from '../models/User';
 
 export class ReportController {
   static async show(req: Request, res: Response) {
-    const users = await User.findAll({
-      attributes: ['name', 'email'],
-      where: {
-        email: {
-          [Op.iLike]: '%@rocketseat.com.br'
-        }
-      },
-      include: [
-        {
-          association: 'addresses',
-          where: { street: 'Rua Guilherme Gembala' }
-        },
-        {
-          association: 'techs',
-          required: false,
-          where: {
-            name: { [Op.iLike]: 'React%' }
+    try {
+      const users = await User.findAll({
+        attributes: ['name', 'email'],
+        where: {
+          email: {
+            [Op.iLike]: '%@rocketseat.com.br'
           }
-        }
-      ]
-    });
-    return res.json(users);
+        },
+        include: [
+          {
+            association: 'addresses',
+            where: { street: 'Rua Guilherme Gembala' }
+          },
+          {
+            association: 'techs',
+            required: false,
+            through: { attributes: [] },
+            where: {
+              name: { [Op.iLike]: 'React%' }
+            }
+          }
+        ]
+      });
+      return res.json(users);
+    } catch (error) {
+      res.status(400).json({ error: error });
+    }
   }
 }
