a6f995c36a93002da8dacbbb88dcfae7258c8466
Gabriel dos Santos Oliveira <gabrielsantosoliveira951@gmail.com>
2023-01-06 17:05:34 -0400

    feat: Add try catch and validation

--- src/controllers/TechController.ts
+++ src/controllers/TechController.ts
@@ -2,50 +2,71 @@ import { Request, Response } from 'express';
 
 import { Tech } from '../models/Tech';
 import { User } from '../models/User';
+import { schemaTechParamsStore } from '../validation/Tech/index';
+import { schemaTechBodyStore } from '../validation/Tech/store';
 export class TechController {
   static async index(req: Request, res: Response) {
-    const { user_id } = req.params;
-    const user = await User.findByPk(user_id, {
-      include: {
-        association: 'techs',
-        through: { attributes: [] }
-      }
-    });
-    return res.json(user?.techs);
+    try {
+      const { user_id } = schemaTechParamsStore.parse(
+        req.params
+      );
+      const user = await User.findByPk(user_id, {
+        include: {
+          association: 'techs',
+          through: { attributes: [] }
+        }
+      });
+      return res.json(user?.techs);
+    } catch (error) {
+      return res.status(400).json({ error: error });
+    }
   }
   static async store(req: Request, res: Response) {
-    const { user_id } = req.params;
-    const { name } = req.body;
-    const user = await User.findByPk(user_id);
-    if (!user) {
-      return res
-        .status(400)
-        .json({ error: 'User not found' });
+    try {
+      const { user_id } = schemaTechParamsStore.parse(
+        req.params
+      );
+      const { name } = schemaTechBodyStore.parse(req.body);
+      const user = await User.findByPk(user_id);
+      if (!user) {
+        return res
+          .status(400)
+          .json({ error: 'User not found' });
+      }
+      const [tech] = await Tech.findOrCreate({
+        where: { name }
+      });
+      await user.addTech(tech, {});
+
+      return res.json(tech);
+    } catch (error) {
+      return res.status(400).json({ error: error });
     }
-    const [tech, created] = await Tech.findOrCreate({
-      where: { name }
-    });
-    await user.addTech(tech);
-    return res.json(tech);
   }
   static async delete(req: Request, res: Response) {
-    const { user_id } = req.params;
-    const { name } = req.body;
-    const user = await User.findByPk(user_id);
-    if (!user) {
-      return res
-        .status(400)
-        .json({ error: 'User not found' });
+    try {
+      const { user_id } = schemaTechParamsStore.parse(
+        req.params
+      );
+      const { name } = schemaTechBodyStore.parse(req.body);
+      const user = await User.findByPk(user_id);
+      if (!user) {
+        return res
+          .status(400)
+          .json({ error: 'User not found' });
+      }
+      const tech = await Tech.findOne({
+        where: { name }
+      });
+      if (!tech) {
+        return res
+          .status(400)
+          .json({ error: 'Tech not found' });
+      }
+      await user.removeTech(tech);
+      return res.json();
+    } catch (error) {
+      return res.status(400).json({ error: error });
     }
-    const tech = await Tech.findOne({
-      where: { name }
-    });
-    if (!tech) {
-      return res
-        .status(400)
-        .json({ error: 'Tech not found' });
-    }
-    await user.removeTech(tech);
-    return res.json();
   }
 }
