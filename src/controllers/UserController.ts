b022a721c5d5f037a3a5a4493c8a5a924ef02b09
Gabriel dos Santos Oliveira <gabrielsantosoliveira951@gmail.com>
2023-01-06 17:05:47 -0400

    feat: Add try catch and validation

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -1,14 +1,25 @@
 import { Request, Response } from 'express';
 
 import { User } from '../models/User';
+import { schemaUserBodyStore } from '../validation/User/store';
 export class UserController {
   static async store(req: Request, res: Response) {
-    const { email, name } = req.body;
-    const user = await User.create({ email, name });
-    return res.json(user);
+    try {
+      const { email, name } = schemaUserBodyStore.parse(
+        req.body
+      );
+      const user = await User.create({ email, name });
+      return res.json(user);
+    } catch (error) {
+      return res.status(400).json({ error: error });
+    }
   }
   static async findAll(req: Request, res: Response) {
-    const users = await User.findAll();
-    return res.json(users);
+    try {
+      const users = await User.findAll();
+      return res.json(users);
+    } catch (error) {
+      return res.status(400).json({ error: error });
+    }
   }
 }
