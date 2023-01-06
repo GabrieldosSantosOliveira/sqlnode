b8172dcb097d52aca6ad49453d3f253b7bc3a8e0
Gabriel dos Santos Oliveira <gabrielsantosoliveira951@gmail.com>
2023-01-06 17:02:59 -0400

    feat: Add cors, swagger for documentation and morgan for logger

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,31 @@
+import cors from 'cors';
 import express from 'express';
+import helmet from 'helmet';
+import morgan from 'morgan';
+import SwaggerUi from 'swagger-ui-express';
+
+import { routes } from './routes';
+import SwaggerSettings from './swagger.json';
 
 import 'dotenv/config';
-import { routes } from './routes';
 import './database';
 const app = express();
+
 app.use(express.json());
+app.use(cors());
+app.use(helmet());
+app.use(morgan('dev'));
+app.use(
+  '/api-docs',
+  SwaggerUi.serve,
+  SwaggerUi.setup(SwaggerSettings)
+);
+
 app.use(routes);
-const PORT = process.env.PORT ?? 3333;
+app.use((req, res) => {
+  return res.status(404).json({ error: 'Page not Found' });
+});
+const PORT = process.env.PORT || 3333;
 app.listen(PORT, () => {
   console.log(`http://localhost:${PORT}`);
 });
